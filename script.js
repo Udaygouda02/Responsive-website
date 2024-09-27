4d221b7fc09f71fd5ef36049cf244b994aef914f
Udaygouda02 <udaygouda02@gmail.com>
2024-09-27 10:16:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,99 @@
+var crsr=document.querySelector("#cursor")
+var blur=document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x + "px"
+    crsr.style.top=dets.y + "px"
+    blur.style.left = dets.x - 250+"px"
+    blur.style.top=dets.y - 250+"px"
+})
+
+gsap.to("#nav", {
+    backgroundColor:"#000",
+    duration:0.5,
+    height:"110px" ,
+     scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+      
+        start:"top -11%",
+        scrub:1
+     }      
+} ) 
+
+gsap.to("#main",{
+    backgroundColor: "#000" ,
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+      
+        start:"top -25vh",
+        end:"top -70%",
+        scrub:2
+    }
+})
+
+var h4all= document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale = 3
+        crsr.style.border="0.5px solid #fff"
+        crsr.style.backgroundColor="transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1
+        crsr.style.border="0px solid  #95C11E"
+        crsr.style.backgroundColor=" #95C11E"
+    })
+})
+
+gsap.from("#about-us img,#about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        
+        start:"top 60%",
+        end:"top 55%",
+        scrub:3
+    }
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+gsap.from("#page4 h1",{ 
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        
+        start:"top 75%",
+        end:"top 70%",
+        scrub:3
+    }
+}  )  
